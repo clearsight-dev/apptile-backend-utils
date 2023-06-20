6e80f37f67352433b98c7a967b4dd23242b50296
Abhishek Arora <abhishek.arora@apptile.io>
2023-06-20 15:41:18 +0530

    reverted to uuid v4

--- src/utils/requestTracer.ts
+++ src/utils/requestTracer.ts
@@ -1,11 +1,14 @@
 import cls from 'cls-hooked';
 import {NAMESPACE_LOG_TRACE, NAMESPACE_LOG_TRACE_KEY} from '../constants';
-import {nanoid} from 'nanoid';
+// import {customAlphabet} from 'nanoid';
+import {v4 as uuid} from 'uuid';
 
 export const requestTracingNamespace = cls.createNamespace(NAMESPACE_LOG_TRACE);
+// const alphaNumeric = '0123456789abcdefghijklmnopqrstuvwxyz';
+// const nanoid = customAlphabet(alphaNumeric, 10);
 
 export const generateTraceId = () => {
-  return nanoid(10);
+  return uuid();
 };
 
 export const getTracingId = () => {
